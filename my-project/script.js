0a9ffe1b1d9e40620aed1b547e48ad0cf2eb3ab8
Bryant Martinez <bryant.e.martinez@outlook.com>
2025-03-11 23:57:00 -0700

    commit 1

--- my-project/script.js
+++ my-project/script.js
@@ -1,1 +1,12 @@
-// To-Do
+const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NDUzZWIwZmQ1N2QzODllMjRlMGFhNTY1MDA5NjA2MCIsIm5iZiI6MTc0MTc1NzEzNi4zNzIsInN1YiI6IjY3ZDExYWQwNDM0Yzk4YzhlYzgxNWU2NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.tuFhCncKvmXShNl3qsg2SMy0fRjgsuSvoy2q-aetQmA'
+    }
+  };
+  
+  fetch('https://api.themoviedb.org/3/authentication', options)
+    .then(res => res.json())
+    .then(res => console.log(res))
+    .catch(err => console.error(err));
